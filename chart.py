89641568f6ac85cd388f34d615641485885b6608
Matthew <mwbond@gmail.com>
2015-06-26 13:51:48 -0400

    Debugged chart.py

--- chart.py
+++ chart.py
@@ -14,26 +14,25 @@ class Chart():
 
     def add_labels(self, col_names=None, row_names=None):
         if col_names is not None:
-            for row in self.data:
-                if len(row) == len(col_names):
-                    self.data = [col_names] + self.data
-                    self.has_col_names = True
-                    row_names = [''] + row_names
+            if all([len(row) == len(col_names) for row in self.data]):
+                self.data = [col_names] + self.data
+                self.has_col_names = True
+                row_names = [''] + row_names
         if (row_names is not None) and (len(self.data) == len(row_names)):
-            for row, name in zip(self.data, row_names):
-                row = [name] + row
+            for index, name in enumerate(row_names):
+                self.data[index] = [name] + self.data[index]
             self.has_row_names = True
 
     def get_line(self, r):
         line = []
         for w, cell in zip(self.widths, r):
-            line.append(cell.ljust(w, ' '))
-        if self.has_row_name:
-            return line + ' || ' + ' | '.join(line[1:])
+            line.append(cell.rjust(w, ' '))
+        if self.has_row_names:
+            return line[0] + ' || ' + ' | '.join(line[1:])
         return ' | '.join(line)
 
     def get_headers(self):
-        table_width = sum(self.widths) + 3 * (len(self.widths) - 1)
+        table_width = sum(self.widths) + 3 * (len(self.widths) - 1) + 1
         sep = '-' * table_width
         headers = self.get_line(self.data[0])
         return '\n'.join([sep, headers, sep])
@@ -42,9 +41,9 @@ class Chart():
         self.update_widths()
         for count, row in enumerate(self.data):
             if (self.has_col_names) and (count % col_names_rpt == 0):
-                print(get_headers)
+                print(self.get_headers())
             if (not self.has_col_names) or (count != 0):
-                print(self.create_line(row))
+                print(self.get_line(row))
 
     def reset(self):
         self.data = []
