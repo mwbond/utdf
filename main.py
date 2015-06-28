0398074284ca0aaf7e4f2e4c2521c8d037cac327
Matthew <mwbond@gmail.com>
2015-06-28 11:38:17 -0400

    Build sqlite db from csv files now works.

--- main.py
+++ main.py
@@ -3,29 +3,19 @@
 
 import os
 
-import chart
 import utdf
+
 
 def main():
     folder = 'Data'
-    old, new = None, None
     for f_name in os.listdir(folder):
         if f_name[-4:] == '.csv':
             f_path = os.path.join(folder, f_name)
             db_path = f_path[:-3] + 'db'
-            network = utdf.UTDF(db_path, f_path)
-            break
-            '''if old is not None:
-                for key in new.sections:
-                    a = new.sections[key]
-                    b = old.sections[key]
-                    output = a.diff_rows(b)
-                    if any(output):
-                        print(a.row_names)
-                        print(b.row_names)
-                        print(output)
-                        input('Press Enter')'''
-            old = new
+            attributes = utdf.build_db(db_path, f_path)
+            u = utdf.UTDF(db_path, attributes)
+            print(u.correct_version())
+            utdf.save_table_to_csv(db_path, 'Phases')
 
 if __name__ == '__main__':
     main()
