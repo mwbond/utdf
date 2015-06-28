46a53387348a159ea83093081829ee48c98539c7
Matthew <mwbond@gmail.com>
2015-06-28 17:49:08 -0400

    Added basic compare function.

--- src/main.py
+++ src/main.py
@@ -4,17 +4,21 @@
 import os
 
 import utdf.build
+import utdf.utdf
 
 
 def main():
     folder = os.path.join(os.pardir, 'data')
+    old = None
     for f_name in os.listdir(folder):
         if f_name[-4:] == '.csv':
             f_path = os.path.join(folder, f_name)
             db_path = f_path[:-3] + 'db'
-            print(db_path)
-            utdf.build.build_db(db_path, f_path)
-            utdf.build.save_table_to_csv(db_path, 'Network')
+            # utdf.build.build_db(db_path, f_path)
+            # utdf.build.save_table_to_csv(db_path, 'Network')
+            if old is not None:
+                utdf.utdf.compare_nodes(db_path, old)
+            old = db_path
 
 if __name__ == '__main__':
     main()
--- src/utdf/utdf.py
+++ src/utdf/utdf.py
@@ -2,12 +2,31 @@
 # Comparing and analyzing UTDF sqlite dbs
 
 
-class UTDF():
-    UTDF_VERSION = '8'
+import sqlite3
 
-    def __init__(self, db_path, attributes):
-        self.db = db_path
-        self.attributes = attributes
 
-    def correct_version(self, version=UTDF_VERSION):
-        return version == self.attributes['UTDFVERSION']
+def compare(results, key=None):
+    union, intersection = set(), None
+    for db_path in results:
+        if key is not None:
+            results[db_path] = [row[key] for row in results[db_path]]
+        union = union | set(results[db_path])
+        if intersection is None:
+            intersection = set(results[db_path])
+        intersection = intersection & set(results[db_path])
+    for db_path in results:
+        results[db_path] = set(results[db_path]) - intersection
+    return union, intersection, results
+
+
+def compare_nodes(*args):
+    cmd, results = 'SELECT intid, type, x, y, z FROM Nodes', {}
+    for db_path in args:
+        results[db_path] = []
+        conn = sqlite3.connect(db_path)
+        cur = conn.cursor()
+        cur.execute(cmd)
+        for row in cur.fetchall():
+            results[db_path].append(row)
+        conn.close()
+    return compare(results, None)
